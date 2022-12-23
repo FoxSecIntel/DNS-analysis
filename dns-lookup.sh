2db469a07ccbe30b843d6995790bb6a2ab700cd7
13gbc <118615125+13gbc@users.noreply.github.com>
2022-12-23 13:58:58 +0100

    Create dns-lookup.sh

---
+++ dns-lookup.sh
@@ -0,0 +1,16 @@
+# Check if a command line parameter was provided
+if [ $# -eq 0 ]; then
+    # If no parameter was provided, display an error message and exit
+    echo "Error: No domain name provided."
+    exit 1
+fi
+
+# Read in the names from the file
+while read name; do
+    # Append the command line parameter to the name
+    name="$name.$1"
+
+    # Perform a DNS lookup on the name using the host command
+    # and redirect the output to a file
+    host "$name" >> dns_lookup_results.txt
+done < names.txt
